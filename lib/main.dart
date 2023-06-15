b2f3343cef7c9bc2928efd39b6c85421bfd20dbc
Yara Horany <yara.horany1@gmail.com>
2023-06-15 12:17:36 +0300

    Adding the game screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:quoridor/screens/game_screen/game_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,9 +11,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
+      title: 'Quoridor Board Game',
       debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+      initialRoute: '/',
+      getPages: [
+        GetPage(
+          name: '/',
+          page: () => const GameScreen(),
+        ),
+      ],
     );
   }
 }
